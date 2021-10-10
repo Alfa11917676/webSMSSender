6367d4d13433c5d91cb8e251df8d74c49ea397a3
Arnab11917676 <alfatitas747ray@outlook.com>
2021-10-10 21:29:31 +0530

    Done

--- smsSender.py
+++ smsSender.py
@@ -20,31 +20,21 @@ def send_sms(number, message):
     return dic.get('return')
 
 def btn_click():
-    df = pd.read_csv('ph_nos.csv',index_col=None)
-    val = (df['919477855936'])
-    val = str(val)
-    if val[0]=='9' and val[1]=='1':
-        val = '+'+val
-    print(val)
-    df = df.to_json()
-    print (type(df))
-    print (df)
-
-    # for index, number in df.iterrows():
-    # # msg= textMsg.get("1.0", END)
-    # # number  = textNumber.get()
-    # # r = send_sms(number, msg)
-    #     print( number)
-    #     msg = textMsg.get("1.0", END)
-    #     r = send_sms(number, msg)
-    #     i = 0
-    #     if r:
-    #         showinfo("Send Success", "Successfully sent")
-    #         i += 1
-    #
-    #     else:
-    #         showerror("Error", f"Something went wrong with {number}..")
-
+    df = pd.read_csv('ph_nos.csv')
+    val = (df['phoneNumber'])
+    i = 0
+    for numbers in val:
+        num = str(numbers)
+        if num[0]=='9' and num[1]=='1':
+            num = '+'+num
+            num = num[3:]
+        msg = textMsg.get("1.0", END)
+        r = send_sms(num, msg)
+        if r:
+                        i += 1
+        else:
+            showerror("Error", f"Something went wrong with {num}..")
+    print (i)
 root = Tk()
 root.title("Message Sender ")
 root.geometry("400x550")

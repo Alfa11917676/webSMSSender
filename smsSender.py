84c724f5997e6e331f3940c11c3d8f54829f0e5c
Arnab11917676 <alfatitas747ray@outlook.com>
2021-10-10 11:24:26 +0530

    Done

--- smsSender.py
+++ smsSender.py
@@ -20,17 +20,30 @@ def send_sms(number, message):
     return dic.get('return')
 
 def btn_click():
-    df = pd.read_csv('ph_nos.csv')
-    num = df['0']
-    msg = textMsg.get("1.0", END)
-    r = send_sms(num, msg)
-    i = 0
-    if r:
-        showinfo("Send Success", "Successfully sent")
-        i += 1
-        print(i)
-    else:
-        showerror("Error", "Something went wrong..")
+    df = pd.read_csv('ph_nos.csv',index_col=None)
+    val = (df['919477855936'])
+    val = str(val)
+    if val[0]=='9' and val[1]=='1':
+        val = '+'+val
+    print(val)
+    df = df.to_json()
+    print (type(df))
+    print (df)
+
+    # for index, number in df.iterrows():
+    # # msg= textMsg.get("1.0", END)
+    # # number  = textNumber.get()
+    # # r = send_sms(number, msg)
+    #     print( number)
+    #     msg = textMsg.get("1.0", END)
+    #     r = send_sms(number, msg)
+    #     i = 0
+    #     if r:
+    #         showinfo("Send Success", "Successfully sent")
+    #         i += 1
+    #
+    #     else:
+    #         showerror("Error", f"Something went wrong with {number}..")
 
 root = Tk()
 root.title("Message Sender ")
